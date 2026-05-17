7a2fdaa24abf2732e33a1935b794af8a727f2f31
wnsnk <236621219+wnsnk@users.noreply.github.com>
2026-05-17 22:42:35 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
+from wtforms import StringField, PasswordField, SubmitField, RadioField, SelectField, SelectMultipleField, BooleanField, SearchField
+from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__)
+app.secret_key = 'super secret key'
+Bootstrap = Bootstrap5(app=app)
+
+
+@app.route("/")
+def homepage():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
