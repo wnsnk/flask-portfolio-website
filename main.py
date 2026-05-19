c6bb5f6d98d364599864d1867c5916a5b67258e9
wnsnk <236621219+wnsnk@users.noreply.github.com>
2026-05-19 19:51:10 +0200

    changed name of scraper/ quick backup

--- main.py
+++ main.py
@@ -4,20 +4,32 @@ from flask_wtf.csrf import CSRFProtect
 from wtforms import StringField, PasswordField, SubmitField, RadioField, SelectField, SelectMultipleField, BooleanField, SearchField
 from wtforms.validators import DataRequired, Email, Length
 from flask_bootstrap import Bootstrap5
+from dotenv import load_dotenv
+import os
+
+from github_scraper import GithubRepositoryNameScraper
+
+load_dotenv()
+
+FIRST_NAME = os.getenv('FIRST_NAME')
+LAST_NAME = os.getenv('LAST_NAME')
+OCCUPATION = os.getenv('OCCUPATION')
+GITHUB_USERNAME = os.getenv('GITHUB_USERNAME')
 
 
-NAME = 'name'
-OCCUPATION = 'job'
-GITHUB_LINK = 'https://github.com/wnsnk'
+full_name = f'{FIRST_NAME} {LAST_NAME}'
+github_url = f'https://github.com/{GITHUB_USERNAME}'
+
+github_scraper = GithubRepositoryNameScraper(GITHUB_USERNAME)
 
 app = Flask(__name__)
-app.secret_key = 'super secret key'
+app.secret_key = os.getenv('FLASK_SECRET_KEY')
 Bootstrap = Bootstrap5(app=app)
 
 
 @app.route("/")
 def homepage():
-    return render_template('index.html', name=NAME, occupation=OCCUPATION, github=GITHUB_LINK)
+    return render_template('index.html', name=full_name, occupation=OCCUPATION, github=github_url)
 
 
 if __name__ == '__main__':
